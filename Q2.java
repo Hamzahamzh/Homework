64a254332fdce91d41562e4822ba7104bebe5f47
Hamzahamzh <112499089+Hamzahamzh@users.noreply.github.com>
2022-09-28 10:04:39 +0300

    Homework 1

---
+++ Q2.java
@@ -0,0 +1,13 @@
+public class Q2 {
+    public static void main(String[] args) {
+        int i;
+        int x[]={1,2,3,4,5,6,7,8,9,10};
+        int sum=0;
+        for(i=0;i<10;i++)
+            sum = sum + x[i];
+
+        System.out.print(" sum is:");
+        System.out.print( sum );
+    }
+
+}
---
+++ Q6.java
@@ -0,0 +1,19 @@
+public class Q6 {
+
+    public static void main(String[] args) {
+
+        System.out.println("             +            ");
+        System.out.println("           +   +          ");
+        System.out.println("         +       +        ");
+        System.out.println("       +-----------+      ");
+        System.out.println("       |    .-.    |      ");
+        System.out.println("       |    | |    |      ");
+        System.out.println("       +-+-+-+-+-+-+      ");
+        System.out.println("                          ");
+
+
+
+
+    }
+
+}
